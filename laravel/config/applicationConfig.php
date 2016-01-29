d64b6583911c3e72c10bd2b58967e38b3d2c41fb
theminer3746 <bwsminecraft@hotmail.com>
2016-01-29 12:53:31 +0700

    Add configuration file

---
+++ laravel/config/applicationConfig.php
@@ -0,0 +1,47 @@
+<?php
+
+return [
+
+  /*
+  release: release notation & software state
+  values: alpha, beta, release
+  */
+  'release' => 'alpha',
+
+  /*
+  mode: application operating mode
+  values: confirmation, audition, sorting1, sorting2, war, close
+
+  confirmation => confirm that user will be in the same club as before
+  audition => audition new member
+  sorting1 => user select the sequence and we'll random it out
+  sorting2 => phase 2 of sorting, this will give the user to decide whether or not
+    they accept the club that we give them
+  war => get in to reg war
+  close => close
+  */
+  'mode' => 'confirmation',
+
+  /*
+  TUSD Server Address
+  */
+  'tusd_address' => '',
+
+  /*
+  TUSD API key and secret
+  */
+  'tusd_api_key' => '',
+  'tusd_api_secret' => '',
+
+  /*
+  reCAPTCHA Verification URL : URL for the Google reCAPTCHA API service
+  */
+  'recaptcha_verification_url' => 'https://www.google.com/recaptcha/api/siteverify',
+
+  /*
+  reCAPTCHA Secret : Secret (API Key) for the reCAPTCHA service
+  */
+  'recaptcha_secret' => '6LdeihYTAAAAAHic28L24vHAgEaJM8EH_WwdifUd',
+
+
+];
