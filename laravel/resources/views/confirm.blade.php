e3cf51f626cdd98d3a28cb2dfaae6194dfb2e9b8
theminer3746 <bwsminecraft@hotmail.com>
2016-01-31 21:31:25 +0700

    Create confirmation blade

---
+++ laravel/resources/views/confirm.blade.php
@@ -0,0 +1,25 @@
+@extends('layout.main')
+
+@section('content')
+<form class="form-horizontal" method="POST" action="/confirm/go">
+  <fieldset>
+   <legend>ยืนยันการลงทะเบียนชมรมเดิม</legend>
+   <span>ชมรมปัจจุบัน : {{ $data['current_club'] }}</span><br>
+   <span>สถานะ :
+     @if($data['confirmation_status'] == 1)
+      ยืนยันการลงทะเบียนแล้ว
+     @else
+      ยังไม่ได้ยืนยันการลงทะเบียน
+     @endif
+   </span>
+   {{ csrf_field() }}
+   <br />
+
+   <div class="form-group">
+     <div class="col-lg-12">
+       <button type="submit" class="btn btn-success btn-block">ยืนยัน</button>
+     </div>
+   </div>
+  </fieldset>
+</form>
+@stop
