35b9fb32221b157e5f854e2020029cdbc383fa30
theminer3746 <bwsminecraft@hotmail.com>
2016-01-31 21:33:09 +0700

    Create handler for confirmation page

--- laravel/app/Http/Controllers/ConfirmationController.php
+++ laravel/app/Http/Controllers/ConfirmationController.php
@@ -1,9 +1,27 @@
 <?php namespace App\Http\Controllers;
 
 use DB;
+use Session;
+use Illuminate\Routing\Controller;
 
 class ConfirmationController extends Controller{
+
+  /**
+   * Show confirmation page
+   *
+   * @return view on success
+   */
   public function showConfirmationPage(){
-    
+    if(OperationController::userLoggedIn()){
+      $data = DB::table('user')->where('national_id',Session::get('national_id'))->first();
+      return view('confirm')
+        ->with('data',array(
+          'fullname' => Session::get('fullname'),
+          'confirmation_status' => $data->confirmation_status,
+          'current_club' => $data->current_club
+        ));
+    }else{
+      Redirect::to('error/notloggedin');
+    }
   }
 }
