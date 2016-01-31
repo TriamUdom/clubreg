8d3d03deb2bd47e2f99538cc70494a1fca17074a
theminer3746 <bwsminecraft@hotmail.com>
2016-01-31 23:03:06 +0700

    Add placeholder data to index

--- laravel/resources/views/index.blade.php
+++ laravel/resources/views/index.blade.php
@@ -1,1 +1,15 @@
 @extends('layout.main')
+
+@section('content')
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+<br><br>
+@if(Session::get('logged_in') == 1)
+<a href="/confirm">
+  <button class="btn btn-success btn-block">ดำเนินการต่อ</button>
+</a>
+@else
+<a href="/login">
+  <button class="btn btn-success btn-block">ดำเนินการต่อ</button>
+</a>
+@endif
+@stop
