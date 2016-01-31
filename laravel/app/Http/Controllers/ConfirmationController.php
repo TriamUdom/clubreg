f41872f5e8008f4b0685440eb1d702b37ef81e78
theminer3746 <bwsminecraft@hotmail.com>
2016-01-31 22:26:59 +0700

    Add confirm handler

--- laravel/app/Http/Controllers/ConfirmationController.php
+++ laravel/app/Http/Controllers/ConfirmationController.php
@@ -1,7 +1,9 @@
 <?php namespace App\Http\Controllers;
 
 use DB;
+use Input;
 use Session;
+use Redirect;
 use Illuminate\Routing\Controller;
 
 class ConfirmationController extends Controller{
@@ -14,14 +16,52 @@ class ConfirmationController extends Controller{
   public function showConfirmationPage(){
     if(OperationController::userLoggedIn()){
       $data = DB::table('user')->where('national_id',Session::get('national_id'))->first();
+      $current_club = DB::table('club')->where('club_code',$data->current_club)->pluck('club_name');
       return view('confirm')
         ->with('data',array(
           'fullname' => Session::get('fullname'),
           'confirmation_status' => $data->confirmation_status,
-          'current_club' => $data->current_club
+          'current_club' => $current_club
         ));
     }else{
-      Redirect::to('error/notloggedin');
+      return Redirect::to('error/notloggedin');
+    }
+  }
+
+  /**
+   * Confirm handler
+   *
+   * @return Redirection
+   */
+  public function confirm(){
+    $current_status = Input::get('current_status');
+
+    if($current_status == 1){
+      try{
+        if(DB::table('user')->where('national_id',Session::get('national_id'))->exists()){
+          DB::table('user')->where('national_id',Session::get('national_id'))->update(array(
+            'confirmation_status' => 0
+          ));
+          return Redirect::back();
+        }else{
+          throw new Exception('No national_id');
+        }
+      }catch(\Exception $e){
+        return Redirect::to('error/notloggedin');
+      }
+    }else{
+      try{
+        if(DB::table('user')->where('national_id',Session::get('national_id'))->exists()){
+          DB::table('user')->where('national_id',Session::get('national_id'))->update(array(
+            'confirmation_status' => 1
+          ));
+          return Redirect::back();
+        }else{
+          throw new Exception('No national_id');
+        }
+      }catch(\Exception $e){
+        return Redirect::to('error/notloggedin');
+      }
     }
   }
 }
