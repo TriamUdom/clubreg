65b2e2dcef2e4851209f1c5eaba5e373b132ff5e
theminer3746 <bwsminecraft@hotmail.com>
2016-02-01 10:47:03 +0700

    Add announcement for each mode

--- laravel/resources/views/index.blade.php
+++ laravel/resources/views/index.blade.php
@@ -1,7 +1,21 @@
 @extends('layout.main')
 
 @section('content')
-Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+@if(Config::get('applicationConfig.mode') == 'confirmation')
+<legend>การลงทะเบียนชมรมเดิม</legend>
+นักเรียนที่ต้องการลงทะเบียนชมรมเดิม ลงทะเบียนได้ตั้งแต่วันที่ 2 ถึงวันที่ 4 กุมภาพันธ์ 2559<br>
+สำหรับนักเรียนที่ต้องการเปลี่ยนชมรมห้ามยืนยัน และให้รอประกาศจากโรงเรียนเตรียมอุดมศึกษาช่วงเปิดภาคเรียนที่ 1 ปีการศึกษา 2559
+@elseif(Config::get('applicationConfig.mode') == 'audition')
+
+@elseif(Config::get('applicationConfig.mode') == 'sorting1')
+
+@elseif(Config::get('applicationConfig.mode') == 'sorting2')
+
+@elseif(Config::get('applicationConfig.mode') == 'war')
+
+@elseif(Config::get('applicationConfig.mode') == 'close')
+
+@endif
 <br><br>
 @if(Session::get('logged_in') == 1)
 <a href="/confirm">
