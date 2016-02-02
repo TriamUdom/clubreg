99125d91d0ccc641a6d2ca1153dfa5c910a20eb7
theminer3746 <bwsminecraft@hotmail.com>
2016-02-02 11:58:54 +0700

    We're now release V1....

--- laravel/config/applicationConfig.php
+++ laravel/config/applicationConfig.php
@@ -6,7 +6,7 @@ return [
   release: release notation & software state
   values: alpha, beta, release
   */
-  'release' => 'alpha',
+  'release' => 'release',
 
   /*
   mode: application operating mode
