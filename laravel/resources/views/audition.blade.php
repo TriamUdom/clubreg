83151834c4f5ab34108245b43a7f8437ca7d694a
theminer3746 <bwsminecraft@hotmail.com>
2016-02-05 01:22:04 +0700

    Create audition blade

---
+++ laravel/resources/views/audition.blade.php
@@ -0,0 +1,28 @@
+@extends('layout.main')
+
+@section('content')
+<table class="table table-striped table-hover ">
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>ชมรม</th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+    @for ($i = 0; $i < count($data); $i++)
+      <form class="form-horizontal" method="POST" action="/president/audition.do">
+        <fieldset>
+          <tr>
+            <td>{{ $i+1 }}</td>
+            <td>{{ $data[$i]->club_name }}</td>
+            <td><a href=""><button type="submit" class="btn btn-primary btn-block">เลือก</button></a></td>
+          </tr>
+
+          {{ csrf_field() }}
+        </fieldset>
+      </form>
+    @endfor
+  </tbody>
+</table>
+@stop
