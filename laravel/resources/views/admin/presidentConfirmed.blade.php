5f21e6703ded8357367d29a0dbfdfed444934b01
theminer3746 <bwsminecraft@hotmail.com>
2016-02-04 22:58:07 +0700

    Add section for data display

--- laravel/resources/views/admin/presidentConfirmed.blade.php
+++ laravel/resources/views/admin/presidentConfirmed.blade.php
@@ -2,4 +2,29 @@
 
 @section('content')
 
+<legend>{{ Session::get('fullname') }}</legend>
+
+<table class="table table-striped table-hover ">
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>คำนำ</th>
+      <th>ชื่อ</th>
+      <th>นามสกุล</th>
+      <th>ห้อง</th>
+    </tr>
+  </thead>
+  <tbody>
+    @for ($i = 0; $i < count($data); $i++)
+      <tr>
+        <td>{{ $i+1 }}</td>
+        <td>{{ $data[$i]->title }}</td>
+        <td>{{ $data[$i]->fname }}</td>
+        <td>{{ $data[$i]->lname }}</td>
+        <td>{{ $data[$i]->room }}</td>
+      </tr>
+    @endfor
+  </tbody>
+</table>
+
 @stop
