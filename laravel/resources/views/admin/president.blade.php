c006071bddde31c657c3a01e796a3045ce382825
theminer3746 <bwsminecraft@hotmail.com>
2016-02-04 23:39:33 +0700

    Add audition management page

--- laravel/resources/views/admin/president.blade.php
+++ laravel/resources/views/admin/president.blade.php
@@ -2,4 +2,6 @@
 
 @section('content')
 <a class="btn btn-primary btn-lg btn-block" href="/president/confirmed" target="_self">1. นักเรียนที่ยืนยันชมรมเดิม</a>
+<?php //@TODO add @if to check if this club have audition?>
+<a class="btn btn-primary btn-lg btn-block" href="/president/audition" target="_self">2. นักเรียนที่สมัครออดิชัน</a>
 @stop
