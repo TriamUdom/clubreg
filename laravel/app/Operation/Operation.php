055ecff5efcb58940e438be74385add507e02a93
theminer3746 <bwsminecraft@hotmail.com>
2016-02-11 13:13:56 +0700

    Add operation model

---
+++ laravel/app/Operation/Operation.php
@@ -0,0 +1,105 @@
+<?php namespace App\Operation;
+
+use DB;
+
+class Operation{
+
+  private $loginData = null;
+
+  /**
+   * Check if user exist
+   *
+   * @param int $nationalid
+   * @return bool
+   */
+  public function userExist($nationalid){
+    if(DB::table('user')->where('national_id', $nationalid)->exists()){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  /**
+   * Authenticate user and do necessary operation associate with that
+   *
+   * @param int $sid
+   * @param int $nid
+   * @return bool
+   */
+  public function authenticateUser($sid, $nid){
+    if($this->userExist($nid)){
+      $user = DB::table('user')->where('national_id',$nid)->first();
+      if($user->student_id == $sid){
+        // Auth Successful
+        // Laravel's Session Magic. Do Not Touch.
+        //Session::put('logged_in', '1');
+        //Session::put('national_id', $user->national_id);
+        Session::put('fullname', $user->title . $user->fname . " " . $user->lname);
+
+        $this->loginData = array(
+          'national_id' => $user->national_id
+        );
+
+        // Log the request
+        $ip_address = $_SERVER['REMOTE_ADDR'];
+        $this->logAuthenticationAttempt($nid, $ip_address, "1"); // Success
+
+        return true;
+      }else{
+        // Log the request
+        $ip_address = $_SERVER['REMOTE_ADDR'];
+        $this->logAuthenticationAttempt($nid, $ip_address, "0"); // Login Failed
+
+        return false;
+      }
+    }else{
+      // Log the request
+      $ip_address = $_SERVER['REMOTE_ADDR'];
+      $this->logAuthenticationAttempt($nid, $ip_address, "0"); // Login Failed
+
+      return false;
+    }
+  }
+
+  /**
+   * Log user's login attempt
+   *
+   * @param mixed $nationalid
+   * @param mixed $ip_address
+   * @param bool $success whether or not the attempt success
+   * @return bool
+   */
+  private function logAuthenticationAttempt($nationalid, $ip_address, $success){
+
+      $timestamp = time();
+
+      $id = DB::table('login_log')->insertGetId(array(
+          'unix_timestamp' => $timestamp,
+          'user_nationalid' => $nationalid,
+          'ip_address' => $ip_address,
+          'success' => $success
+      ));
+
+      if ($id != 0) {
+          return true;
+      } else {
+          return false;
+      }
+
+  }
+
+  /**
+   * Check if user already logged in
+   *
+   * @return bool
+   */
+  public function userLoggedIn(){
+    if(!is_null($this->loginData)){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+}
