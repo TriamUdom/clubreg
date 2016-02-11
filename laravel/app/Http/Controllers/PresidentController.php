0ee84eefd5364161c30e6657d126d8fe86f9c1fb
theminer3746 <bwsminecraft@hotmail.com>
2016-02-11 13:15:41 +0700

    Move logic part of the class to be in model instead

--- laravel/app/Http/Controllers/PresidentController.php
+++ laravel/app/Http/Controllers/PresidentController.php
@@ -5,6 +5,7 @@ use Input;
 use Config;
 use Session;
 use Redirect;
+use President;
 use Validator;
 
 /*
@@ -18,6 +19,12 @@ use Validator;
 */
 
 class PresidentController extends Controller{
+
+  private $president;
+
+  public function __construct(){
+    $this->president = new President;
+  }
 
   /**
    * Show president login page
@@ -66,105 +73,10 @@ class PresidentController extends Controller{
       return Redirect::back()->with('error','รูปแบบข้อมูลไม่ถูกต้องหรือมีข้อมูลเป็นค่าว่าง');
     }
 
-    if($this->authenticatePresident($username, $password)){
+    if($this->president->authenticatePresident($username, $password)){
       return Redirect::to('/president');
     }else{
       return Redirect::back()->with('error','ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง');
-    }
-  }
-
-  /**
-   * Authenticate the president
-   *
-   * @param mixed $username
-   * @param mixed $password
-   * @return bool
-   */
-  private function authenticatePresident($username, $password){
-    if($this->presidentExist($username)){
-      $data = DB::table('president')->where('username',$username)->first();
-      if($data->password == $password){
-        // Auth Successful
-        // Laravel's Session Magic. Do Not Touch.
-        Session::put('president_logged_in', '1');
-        //Session::put('username', $user->national_id);
-        $club = DB::table('club')->where('club_code', $data->club_code)->pluck('club_name');
-        Session::put('fullname', $club);
-        Session::put('club_code',$data->club_code);
-
-        // Log the request
-        $ip_address = $_SERVER['REMOTE_ADDR'];
-        $this->logAuthenticationAttempt($username, $ip_address, "1"); // Success
-
-        return true;
-      }else{
-        // Log the request
-        $ip_address = $_SERVER['REMOTE_ADDR'];
-        $this->logAuthenticationAttempt($username, $ip_address, "0"); // Login Failed
-
-        return false;
-      }
-    }else{
-      // Log the request
-      $ip_address = $_SERVER['REMOTE_ADDR'];
-      $this->logAuthenticationAttempt($username, $ip_address, "0"); // Login Failed
-
-      return false;
-    }
-  }
-
-  /**
-   * Check if president exist
-   *
-   * @param mixed $username
-   * @return bool
-   */
-  public function presidentExist($username){
-    if(DB::table('president')->where('username', $username)->exists()){
-      return true;
-    }else{
-      return false;
-    }
-  }
-
-  /**
-   * Log user's login attempt
-   *
-   * @param mixed $nationalid
-   * @param mixed $ip_address
-   * @param bool $success whether or not the attempt success
-   * @return bool
-   */
-
-  private function logAuthenticationAttempt($username, $ip_address, $success){
-
-      $timestamp = time();
-
-      $id = DB::table('login_log')->insertGetId(array(
-          'unix_timestamp' => $timestamp,
-          'user_nationalid' => $username,
-          'ip_address' => $ip_address,
-          'success' => $success
-      ));
-
-      if ($id != 0) {
-          return true;
-      } else {
-          return false;
-      }
-
-  }
-
-  /**
-   * Check if president already logged in
-   *
-   * @return bool
-   */
-  public static function presidentLoggedIn(){
-    if(Session::get('president_logged_in') == 1){
-      return true;
-    }else{
-      return false;
     }
   }
 
@@ -176,7 +88,12 @@ class PresidentController extends Controller{
    */
   public function showConfirmedPage(){
     if(self::presidentLoggedIn()){
-      $data = DB::table('user')->where('confirmation_status', 1)->where('current_club', Session::get('club_code'))->orderBy('room', 'asc')->orderBy('number', 'asc')->get();
+      $data = DB::table('user')
+      ->where('confirmation_status', 1)
+      ->where('current_club', Session::get('club_code'))
+      ->orderBy('room', 'asc')
+      ->orderBy('number', 'asc')
+      ->get();
       return view('admin.presidentConfirmed')->with('data',$data);
     }else{
       return Redirect::to('/president/login');
