3062081e5c5e02da63f489bd051545015c327856
theminer3746 <bwsminecraft@hotmail.com>
2016-02-11 13:16:28 +0700

    Move logic part to model

--- laravel/app/Http/Controllers/OperationController.php
+++ laravel/app/Http/Controllers/OperationController.php
@@ -5,6 +5,7 @@ use Input;
 use Config;
 use Session;
 use Redirect;
+use Operation;
 use Validator;
 
 /*
@@ -18,6 +19,12 @@ use Validator;
 */
 
 class OperationController extends Controller{
+
+  private $operation;
+
+  public function __construct(){
+    $this->operation = new Operation;
+  }
 
   /**
    * Login handler
@@ -43,7 +50,7 @@ class OperationController extends Controller{
       return Redirect::back()->with('error','รูปแบบข้อมูลไม่ถูกต้องหรือมีข้อมูลเป็นค่าว่าง');
     }
 
-    if ($this->authenticateUser($sid, $nid)) {
+    if ($this->operation->authenticateUser($sid, $nid)) {
       switch(Config::get('applicationConfig.mode')){
         case 'confirmation':
           return Redirect::to('/confirm');
@@ -68,99 +75,6 @@ class OperationController extends Controller{
         return Redirect::to('/');
     } else {
         return Redirect::back()->with('error','รหัสประจำตัวนักเรียนหรือรหัสประชาชนไม่ถูกต้อง');
-    }
-  }
-
-  /**
-   * Authenticate user and do necessary operation associate with that
-   *
-   * @param int $sid
-   * @param int $nid
-   * @return bool
-   */
-  private function authenticateUser($sid, $nid){
-    if($this->userExist($nid)){
-      $user = DB::table('user')->where('national_id',$nid)->first();
-      if($user->student_id == $sid){
-        // Auth Successful
-        // Laravel's Session Magic. Do Not Touch.
-        Session::put('logged_in', '1');
-        Session::put('national_id', $user->national_id);
-        Session::put('fullname', $user->title . $user->fname . " " . $user->lname);
-
-        // Log the request
-        $ip_address = $_SERVER['REMOTE_ADDR'];
-        $this->logAuthenticationAttempt($nid, $ip_address, "1"); // Success
-
-        return true;
-      }else{
-        // Log the request
-        $ip_address = $_SERVER['REMOTE_ADDR'];
-        $this->logAuthenticationAttempt($nid, $ip_address, "0"); // Login Failed
-
-        return false;
-      }
-    }else{
-      // Log the request
-      $ip_address = $_SERVER['REMOTE_ADDR'];
-      $this->logAuthenticationAttempt($nid, $ip_address, "0"); // Login Failed
-
-      return false;
-    }
-  }
-
-  /**
-   * Check if user exist
-   *
-   * @param int $nationalid
-   * @return bool
-   */
-  private function userExist($nationalid){
-    if(DB::table('user')->where('national_id', $nationalid)->exists()){
-      return true;
-    }else{
-      return false;
-    }
-  }
-
-  /**
-   * Log user's login attempt
-   *
-   * @param mixed $nationalid
-   * @param mixed $ip_address
-   * @param bool $success whether or not the attempt success
-   * @return bool
-   */
-  private function logAuthenticationAttempt($nationalid, $ip_address, $success){
-
-      $timestamp = time();
-
-      $id = DB::table('login_log')->insertGetId(array(
-          'unix_timestamp' => $timestamp,
-          'user_nationalid' => $nationalid,
-          'ip_address' => $ip_address,
-          'success' => $success
-      ));
-
-      if ($id != 0) {
-          return true;
-      } else {
-          return false;
-      }
-
-  }
-
-  /**
-   * Check if user already logged in
-   *
-   * @return bool
-   */
-  public static function userLoggedIn(){
-    $login_session = Session::get('logged_in');
-    if ($login_session == "1") {
-        return true;
-    } else {
-        return false;
     }
   }
 
