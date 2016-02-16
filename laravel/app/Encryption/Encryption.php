6893b66e237f0607fe36c1ec50e3b1400d1b2b20
theminer3746 <bwsminecraft@hotmail.com>
2016-02-16 22:46:58 +0700

    Deprecate Encryption model

---
+++ laravel/app/Encryption/Encryption.php
@@ -0,0 +1,21 @@
+<?php //DEPREACTED
+/*
+namespace App\Encryption;
+
+class Encryption{
+
+  const Method = 'AES-256-CBC';
+
+  public static function encrypt($data, $key){
+    //$nonceSize = openssl_cipher_iv_length(self::METHOD);
+    //$nonce = openssl_random_pseudo_bytes($nonceSize);
+    $encrypted = openssl_encrypt($data, self::Method, $key);
+
+    return $encrypted;
+  }
+
+  public static function($encrypted, $key){
+
+  }
+}
+*/
