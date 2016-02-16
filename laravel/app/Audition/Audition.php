fe5b7d1a90dc0d92e0d7abca2ca70b301a0ef349
theminer3746 <bwsminecraft@hotmail.com>
2016-02-16 22:49:36 +0700

    Add method to add user to audition queue

--- laravel/app/Audition/Audition.php
+++ laravel/app/Audition/Audition.php
@@ -7,4 +7,15 @@ class Audition{
     $data = DB::table('club')->where('audition',1)->where('active',1)->get();
     return $data;
   }
+
+  public function addUserToQueue($club_code){
+    DB::table('audition')->insert(array(
+      'national_id' => , Session::get('national_id'),
+      'club_code' => $club_code,
+      'status' => 0,
+      'timestamp' => time()
+    ));
+
+    return true;
+  }
 }
