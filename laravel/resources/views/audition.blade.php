e78ec0fdd37d5e68708e92ed2440975ab28d81eb
theminer3746 <bwsminecraft@hotmail.com>
2016-02-25 22:49:41 +0700

    Revert "Reposition the submission form"
    This reverts commit b66b101c5062cfed525038510cf226e83a55e1e9.

--- laravel/resources/views/audition.blade.php
+++ laravel/resources/views/audition.blade.php
@@ -1,9 +1,6 @@
 @extends('layout.main')
 
 @section('content')
-
-@if(!empty($data['selected']))
-<legend>ชมรมที่เลือก</legend>
 <table class="table table-striped table-hover ">
   <thead>
     <tr>
@@ -13,42 +10,15 @@
     </tr>
   </thead>
   <tbody>
-    @for ($i = 0; $i < count($data['selected']); $i++)
-      <form class="form-horizontal" method="POST" action="/audition.delete">
+    @for ($i = 0; $i < count($data); $i++)
+      <form class="form-horizontal" method="POST" action="/president/audition.do">
         <fieldset>
           <tr>
             <td>{{ $i+1 }}</td>
-            <td>{{ $data['selected'][$i]['club_name'] }}</td>
-            <td><a href=""><button type="submit" class="btn btn-danger btn-block">ยกเลิก</button></a></td>
-          </tr>
-          <input type="hidden" name="club_code" value="{{ $data['selected'][$i]['club_code'] }}">
-          {{ csrf_field() }}
-        </fieldset>
-      </form>
-    @endfor
-  </tbody>
-</table>
-@endif
-
-<legend>ชมรมที่เปิดรับ</legend>
-<table class="table table-striped table-hover ">
-  <thead>
-    <tr>
-      <th>#</th>
-      <th>ชมรม</th>
-      <th></th>
-    </tr>
-  </thead>
-  <tbody>
-    @for ($i = 0; $i < count($data['available']); $i++)
-      <form class="form-horizontal" method="POST" action="/audition.do">
-        <fieldset>
-          <tr>
-            <td>{{ $i+1 }}</td>
-            <td>{{ $data['available'][$i]->club_name }}</td>
+            <td>{{ $data[$i]->club_name }}</td>
             <td><a href=""><button type="submit" class="btn btn-primary btn-block">เลือก</button></a></td>
           </tr>
-          <input type="hidden" name="club_code" value="{{ $data['available'][$i]->club_code }}">
+          <input type="hidden" name="club_code" value="{{ $data['club_code'] }}">
           {{ csrf_field() }}
         </fieldset>
       </form>
