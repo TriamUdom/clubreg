443d51e1540c140599e7f1c240dc84d19ce85b95
theminer3746 <bwsminecraft@hotmail.com>
2016-02-26 00:19:00 +0700

    Fix typo

--- laravel/resources/views/admin/presidentAudition.blade.php
+++ laravel/resources/views/admin/presidentAudition.blade.php
@@ -68,19 +68,19 @@
     </tr>
   </thead>
   <tbody>
-    @for ($i = 0; $i < count($data); $i++)
+    @for ($i = 0; $i < count($data2); $i++)
       <tr>
         <td>{{ $i+1 }}</td>
-        <td>{{ $data[$i]->title }}</td>
-        <td>{{ $data[$i]->fname }}</td>
-        <td>{{ $data[$i]->lname }}</td>
-        <td>{{ $data[$i]->room }}</td>
+        <td>{{ $data2[$i]->title }}</td>
+        <td>{{ $data2[$i]->fname }}</td>
+        <td>{{ $data2[$i]->lname }}</td>
+        <td>{{ $data2[$i]->room }}</td>
         <td>
           <form class="form-horizontal" method="POST" action="/president/audition.cancel">
             <fieldset>
-              <input type="hidden" name="national_id" value="{{ $data[$i]->national_id }}">
-              <input type="hidden" name="action" value="dismiss">
-              <button type="submit" class="btn btn-danger btn-block">ปฏิเสธ</button>
+              <input type="hidden" name="national_id" value="{{ $data2[$i]->national_id }}">
+              <input type="hidden" name="action" value="cancel">
+              <button type="submit" class="btn btn-danger btn-block">ยกเลิก</button>
               {{ csrf_field() }}
             </fieldset>
           </form>
