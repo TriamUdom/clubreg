ca4c73f2f64f7aed7059880dd227937d22a2b836
theminer3746 <bwsminecraft@hotmail.com>
2016-02-26 00:19:45 +0700

    Add getAuditionPassed method Finish auditionCancel method

--- laravel/app/President/President.php
+++ laravel/app/President/President.php
@@ -118,6 +118,21 @@ class President{
     return $data;
   }
 
+  public function getAuditionPassed(){
+    $data = DB::table('audition')
+      ->join('user','audition.national_id','=','user.national_id')
+      ->where('audition.club_code',Session::get('club_code'))
+      ->where('status',1)
+      ->select('audition.club_code','user.title','user.fname','user.lname','user.room','user.national_id')
+      ->orderBy('room','asc')
+      ->get();
+    for($i=0;$i<count($data);$i++){
+      $data[$i]->national_id = Crypt::encrypt($data[$i]->national_id);
+    }
+
+    return $data;
+  }
+
   public function auditionAction(){
     $national_id_encrypted  = Input::get('national_id');
     $action                 = Input::get('action');
@@ -162,6 +177,35 @@ class President{
   }
 
   public function auditionCancel(){
-    
+    $national_id_encrypted  = Input::get('national_id');
+    $action                 = Input::get('action');
+
+    try{
+      $national_id = Crypt::decrypt($national_id_encrypted);
+    }catch(DecryptException $e){
+      die('DecryptException');
+    }
+
+    switch($action){
+      case 'cancel':
+        //Get user out of our club
+        DB::table('audition')
+          ->where('national_id', $national_id)
+          ->where('club_code', Session::get('club_code'))
+          ->update(array('status' => 0));
+
+        //Prevent user from attend to other club
+        DB::table('audition')
+          ->where('national_id', $national_id)
+          ->whereNotIn('club_code', array(Session::get('club_code')))
+          ->where('status', -2)
+          ->update(array('status' => 0));
+        return 'cancel';
+      break;
+      default:
+        return false;
+      break;
+    }
+
   }
 }
