ea91eba867b60761c0a6cd947beccb46a3b03a97
theminer3746 <bwsminecraft@hotmail.com>
2016-02-25 22:48:02 +0700

    Display button according to club

--- laravel/resources/views/admin/president.blade.php
+++ laravel/resources/views/admin/president.blade.php
@@ -2,6 +2,9 @@
 
 @section('content')
 <a class="btn btn-primary btn-lg btn-block" href="/president/confirmed" target="_self">1. นักเรียนที่ยืนยันชมรมเดิม</a>
-<?php //@TODO add @if to check if this club have audition?>
+@if($data->audition == 1)
 <a class="btn btn-primary btn-lg btn-block" href="/president/audition" target="_self">2. นักเรียนที่สมัครออดิชัน</a>
+@else
+<a class="btn btn-primary btn-lg btn-block" href="/president/registration" target="_self">2. นักเรียนที่สมัครชมรม</a>
+@endif
 @stop
