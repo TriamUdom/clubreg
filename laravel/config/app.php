d891ec3d53fe922c314cfd57e068f9abbaac09df
theminer3746 <bwsminecraft@hotmail.com>
2016-04-12 00:52:43 +0700

    Add Admin to aliases

--- laravel/config/app.php
+++ laravel/config/app.php
@@ -196,10 +196,11 @@ return [
 
         //<-----------OUR JOB STARTS HERE----------->
 
-        'Audition'   => App\Audition\Audition::class,
+        'Admin'         => App\Admin\Admin::class,
+        'Audition'      => App\Audition\Audition::class,
         'Confirmation'  => App\Confirmation\Confirmation::class,
-        'Operation'  => App\Operation\Operation::class,
-        'President'  => App\President\President::class,
+        'Operation'     => App\Operation\Operation::class,
+        'President'     => App\President\President::class,
 
         //<-----------OUR JOB ENDS HERE----------->
 
