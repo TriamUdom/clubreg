32a72a6abe9279dd3a46a5e87f6510323b1ff182
theminer3746 <bwsminecraft@hotmail.com>
2016-04-12 01:08:35 +0700

    Add method require to log the admin in

--- laravel/app/Admin/Admin.php
+++ laravel/app/Admin/Admin.php
@@ -3,5 +3,87 @@
 use DB;
 
 class Admin{
-  
+
+  /**
+   * Authenticate the admin
+   *
+   * @param mixed $username
+   * @param mixed $password
+   * @return bool
+   */
+  public function authenticateAdmin($username, $password){
+    if($this->adminExist($username)){
+      $data = DB::table('admin')->where('username',$username)->first();
+      if($data->password == $password){
+        // Auth Successful
+        // Laravel's Session Magic. Do Not Touch.
+        //Session::put('username', $user->national_id);
+        Session::put('admin_logged_in', '1');
+        $club = DB::table('club')->where('club_code', $data->club_code)->pluck('club_name');
+        Session::put('fullname', $club);
+        Session::put('club_code',$data->club_code);
+
+
+        // Log the request
+        $ip_address = $_SERVER['REMOTE_ADDR'];
+        $this->logAuthenticationAttempt($username, $ip_address, "1"); // Success
+
+        return true;
+      }else{
+        // Log the request
+        $ip_address = $_SERVER['REMOTE_ADDR'];
+        $this->logAuthenticationAttempt($username, $ip_address, "0"); // Login Failed
+
+        return false;
+      }
+    }else{
+      // Log the request
+      $ip_address = $_SERVER['REMOTE_ADDR'];
+      $this->logAuthenticationAttempt($username, $ip_address, "0"); // Login Failed
+
+      return false;
+    }
+  }
+
+  /**
+   * Log user's login attempt
+   *
+   * @param mixed $nationalid
+   * @param mixed $ip_address
+   * @param bool $success whether or not the attempt success
+   * @return bool
+   */
+  private function logAuthenticationAttempt($username, $ip_address, $success){
+
+      $timestamp = time();
+
+      $id = DB::table('login_log')->insertGetId(array(
+          'unix_timestamp' => $timestamp,
+          'user_nationalid' => $username,
+          'ip_address' => $ip_address,
+          'success' => $success
+      ));
+
+      if ($id != 0) {
+          return true;
+      } else {
+          return false;
+      }
+
+  }
+
+  /**
+   * Check if admin exist
+   *
+   * @param mixed $username
+   * @return bool
+   */
+  public function adminExist($username){
+    if(DB::table('admin')->where('username', $username)->exists()){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
 }
