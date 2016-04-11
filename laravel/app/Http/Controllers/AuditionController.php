123d06863a143cacb8d6b73031a6d04c4ddc1ee0
theminer3746 <bwsminecraft@hotmail.com>
2016-04-11 12:48:18 +0700

    Add checking if the user have club before render the audition page

--- laravel/app/Http/Controllers/AuditionController.php
+++ laravel/app/Http/Controllers/AuditionController.php
@@ -42,14 +42,22 @@ class AuditionController extends Controller{
    */
   public function showAuditionPage(){
     if(Operation::userLoggedIn()){
-      $club_code = $this->audition->haveClub();
-      if($club_code){
-        $club_name = DB::table('club')->where('club_code', $club_code)->pluck('club_name');
-        return view('audition')->with('club_name', $club_name);
+      if($confirmed_club = DB::table('confirmation')->where('national_id', Session::get('national_id'))->first()){
+        //Already confirm club
+        return Redirect::to('/confirmed');
       }else{
-        $available = $this->audition->getAuditionClub();
-        $selected = $this->audition->getSelected();
-        return view('audition')->with('data',array('available' => $available, 'selected' => $selected));
+        //Not yet have club
+        $club_code = $this->audition->haveClub();
+        if($club_code){
+          //Some club had accepted the user
+          $club_name = DB::table('club')->where('club_code', $club_code)->pluck('club_name');
+          return view('audition')->with('club_name', $club_name);
+        }else{
+          //No club have accepted the user
+          $available = $this->audition->getAuditionClub();
+          $selected = $this->audition->getSelected();
+          return view('audition')->with('data',array('available' => $available, 'selected' => $selected));
+        }
       }
     }else{
       return Redirect::to('/login');
