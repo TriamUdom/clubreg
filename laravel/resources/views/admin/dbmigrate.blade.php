6edaeb87bdd38f67469db0dec30a18108e2d1886
theminer3746 <bwsminecraft@hotmail.com>
2016-04-12 17:28:22 +0700

    Add migrate button to dbmigrate view

--- laravel/resources/views/admin/dbmigrate.blade.php
+++ laravel/resources/views/admin/dbmigrate.blade.php
@@ -1,7 +1,23 @@
 @extends('layout.main')
 
 @section('content')
+@if(Session::get('error') != null)
+<div class="alert alert-danger" style="background: #FF0000;">
+  {{ Session::get('error') }}
+</div>
+@endif
+@if(Session::get('success') != null)
+<div class="alert alert-success" style="background: #3ada4f;">
+  {{ Session::get('success') }}
+</div>
+@endif
 <legend>Database Migration</legend>
-<h5>ปีในฐานข้อมูล : </h5>
-<h5>ปีในไฟล์ตั้งค่า : {{ Config::get('applicationConfig.operation_year') }}</h5>
+<h5>ปีในฐานข้อมูล : {{ $data['current_year'] }}</h5>
+<h5>ปีในไฟล์ตั้งค่า : {{ $data['operation_year'] }}</h5>
+<form class="form-horizontal" method="POST" action="/admin/dbmigrate.do">
+  <fieldset>
+    {{ csrf_field() }}
+    <button type="submit" class="btn btn-success btn-block">Migrate</button>
+  </fieldset>
+</form>
 @stop
