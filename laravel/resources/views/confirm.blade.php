43df012ef44b0919cbbb31a4203bba8f82e8dec6
theminer3746 <bwsminecraft@hotmail.com>
2016-04-12 22:59:52 +0700

    Change wording

--- laravel/resources/views/confirm.blade.php
+++ laravel/resources/views/confirm.blade.php
@@ -1,33 +1,6 @@
 @extends('layout.main')
 
 @section('content')
-<form class="form-horizontal" method="POST" action="/confirm.do">
-  <fieldset>
-   <legend>ยืนยันการลงทะเบียนชมรมเดิม</legend>
-   <span>ชมรมปัจจุบัน : {{ $data['current_club'] }}</span><br>
-   <span>สถานะ :
-     @if($data['confirmation_status'] == 1)
-      ยืนยันการลงทะเบียนแล้ว
-     @else
-      ยังไม่ได้ยืนยันการลงทะเบียน
-     @endif
-   </span>
-
-   <br />
-   <br />
-
-   <input type="hidden" value="{{ $data['confirmation_status'] }}" name="current_status">
-
-   <div class="form-group">
-     <div class="col-lg-12">
-       @if($data['confirmation_status'] == 1)
-        <button type="submit" class="btn btn-danger btn-block">ยกเลิกการลงทะเบียน</button>
-       @else
-        <button type="submit" class="btn btn-success btn-block">ยืนยันการลงทะเบียน</button>
-       @endif
-     </div>
-   </div>
-   {{ csrf_field() }}
-  </fieldset>
-</form>
+   <legend>ชมรมที่นักเรียนลงทะเบียน</legend>
+   <h6>ปีการศึกษา {{ $year }} : {{ $club }}</h6><br>
 @stop
