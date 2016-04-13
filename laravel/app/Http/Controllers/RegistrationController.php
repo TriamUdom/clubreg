28d7a8394d44af47a4134ac87fcbc79587babbce
theminer3746 <bwsminecraft@hotmail.com>
2016-04-13 22:59:01 +0700

    Add RegistrationController

---
+++ laravel/app/Http/Controllers/RegistrationController.php
@@ -0,0 +1,15 @@
+<?php namespace App\Http\Controllers;
+
+/*
+|--------------------------------------------------------------------------
+| Registration Controller
+|--------------------------------------------------------------------------
+|
+| This controller handle the rendering of registration page
+| and data flow to Registration model
+|
+*/
+
+class RegistrationController extends Controller{
+  
+}
