bf422fb7200a9e83e18203b04cbfdb2216847de9
theminer3746 <bwsminecraft@hotmail.com>
2016-04-14 23:02:26 +0700

    Add select table

--- laravel/resources/views/registration.blade.php
+++ laravel/resources/views/registration.blade.php
@@ -1,5 +1,29 @@
 @extends('layout.main')
 
 @section('content')
-
+<legend>ชมรมที่เปิดรับ</legend>
+<table class="table table-striped table-hover">
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>ชมรม</th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+    @for ($i = 0; $i < count($data['available']); $i++)
+      <form class="form-horizontal" method="POST" action="/registration.do">
+        <fieldset>
+          <tr>
+            <td>{{ $i+1 }}</td>
+            <td>{{ $data['available'][$i]->club_name }}</td>
+            <td><a href=""><button type="submit" class="btn btn-primary btn-block">เลือก</button></a></td>
+          </tr>
+          <input type="hidden" name="club_code" value="{{ $data['available'][$i]->club_code }}">
+          {{ csrf_field() }}
+        </fieldset>
+      </form>
+    @endfor
+  </tbody>
+</table>
 @stop
