809c6c7cfa8b850ef74e484aea8fa1da325676c9
theminer3746 <bwsminecraft@hotmail.com>
2016-04-15 16:30:55 +0700

    Add method to move yearly user's data to user_year table

--- laravel/app/Http/Controllers/DebugOperation.php
+++ laravel/app/Http/Controllers/DebugOperation.php
@@ -1,6 +1,8 @@
 <?php namespace App\Http\Controllers;
 
 use DB;
+use Admin;
+use Redirect;
 
 /*
 |-----------------------------------|
@@ -10,6 +12,7 @@ use DB;
 class DebugOperation extends Controller{
 
   public function moveData(){
+    if(Admin::adminLoggedIn()){
       $raw = DB::table('user')->where('confirmation_status', 1)->get();
       for($i=0;$i<count($raw);$i++){
         $userSet[] = array(
@@ -26,5 +29,35 @@ class DebugOperation extends Controller{
           'club_code' => $userSet[$j]['club_code']
         ]);
       }
+    }else{
+      abort(401);
+    }
+  }
+
+  public function moveuserdata(){
+    if(Admin::adminLoggedIn()){
+      $raw = DB::table('user')->get();
+      for($i=0;$i<count($raw);$i++){
+        $userSet[] = array(
+          'national_id' => $raw[$i]->national_id,
+          'room' => $raw[$i]->room,
+          'number' => $raw[$i]->number,
+          'year' => 2558,
+          'club_code' => $raw[$i]->current_club
+        );
+      }
+
+      for($j=0;$j<count($userSet);$j++){
+        DB::table('user_year')->where('national_id', $userSet[$j]['national_id'])->insert([
+          'national_id' => $userSet[$j]['national_id'],
+          'room' => $userSet[$j]['room'],
+          'number' => $userSet[$j]['number'],
+          'year' => $userSet[$j]['year'],
+          'club_code' => $userSet[$j]['club_code']
+        ]);
+      }
+    }else{
+      abort(401);
+    }
   }
 }
