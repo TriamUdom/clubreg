570f7e8fa1acb263c4455e4b96e710b455754238
theminer3746 <bwsminecraft@hotmail.com>
2016-04-18 00:47:06 +0700

    Change 404 page to custom 404 error page

--- laravel/app/Exceptions/Handler.php
+++ laravel/app/Exceptions/Handler.php
@@ -42,8 +42,8 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
-        if ($e instanceof ModelNotFoundException) {
-            $e = new NotFoundHttpException($e->getMessage(), $e);
+        if($e instanceof ModelNotFoundException) {
+          abort(404);
         }
 
         if($e instanceof \Illuminate\Session\TokenMismatchException){
