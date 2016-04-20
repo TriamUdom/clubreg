b4d437a612560c03c5f3eb6cd1a4f29966e4ee32
theminer3746 <bwsminecraft@hotmail.com>
2016-04-20 11:45:18 +0700

    Change how getCurrentClub work

--- laravel/app/Confirmation/Confirmation.php
+++ laravel/app/Confirmation/Confirmation.php
@@ -1,8 +1,10 @@
 <?php namespace App\Confirmation;
 
 use DB;
+use Config;
 use Session;
 use Redirect;
+use Operation;
 
 class Confirmation{
 
@@ -14,9 +16,21 @@ class Confirmation{
    * @return array data
    */
   public function getCurrentClub(){
-    $data = DB::table('user')->where('national_id',Session::get('national_id'))->first();
-    $current_club = DB::table('club')->where('club_code',$data->current_club)->pluck('club_name');
-    return array('confirmation_status' => $data->confirmation_status, 'current_club' => $current_club);
+    $club_code = DB::table('user_year')
+                   ->where('national_id', Session::get('national_id'))
+                   ->where('year', Config::get('applicationConfig.operation_year')-1)
+                   ->pluck('club_code');
+    if(!is_null(DB::table('confirmation')->where('national_id', Session::get('national_id'))->where('club_code', $club_code)->first())){
+      $confirmation_status = 1;
+    }else{
+      $confirmation_status = 0;
+    }
+    $current_club = DB::table('club')->where('club_code', $club_code)->pluck('club_name');
+    return array(
+      'confirmation_status' => $confirmation_status,
+      'current_club' => $current_club,
+      'club_code' => $club_code
+    );
   }
 
   /**
