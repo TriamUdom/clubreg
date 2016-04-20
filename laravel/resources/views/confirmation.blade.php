5de363e274e39ac0616d61cfb2107d814d6c9082
theminer3746 <bwsminecraft@hotmail.com>
2016-04-20 11:50:28 +0700

    Change form accord to data

--- laravel/resources/views/confirmation.blade.php
+++ laravel/resources/views/confirmation.blade.php
@@ -1,33 +1,37 @@
 @extends('layout.main')
 
 @section('content')
-<form class="form-horizontal" method="POST" action="/confirm.do">
+
+@include('layout.component.errorwbg')
+
+<legend>ยืนยันการลงทะเบียนชมรมเดิม</legend>
+<span>ชมรมปัจจุบัน : {{ $data['current_club'] }}</span><br>
+<span>สถานะ :
+  @if($data['confirmation_status'] == 1)
+   ยืนยันการลงทะเบียนแล้ว
+  @else
+   ยังไม่ได้ยืนยันการลงทะเบียน
+  @endif
+</span>
+<br />
+<br />
+@if($data['confirmation_status'] == 1)
+<form class="form-horizontal" method="POST" action="/confirmation.delete">
   <fieldset>
-   <legend>ยืนยันการลงทะเบียนชมรมเดิม</legend>
-   <span>ชมรมปัจจุบัน : {{ $data['current_club'] }}</span><br>
-   <span>สถานะ :
-     @if($data['confirmation_status'] == 1)
-      ยืนยันการลงทะเบียนแล้ว
-     @else
-      ยังไม่ได้ยืนยันการลงทะเบียน
-     @endif
-   </span>
-
-   <br />
-   <br />
-
-   <input type="hidden" value="{{ $data['confirmation_status'] }}" name="current_status">
-
-   <div class="form-group">
-     <div class="col-lg-12">
-       @if($data['confirmation_status'] == 1)
-        <button type="submit" class="btn btn-danger btn-block">ยกเลิกการลงทะเบียน</button>
-       @else
-        <button type="submit" class="btn btn-success btn-block">ยืนยันการลงทะเบียน</button>
-       @endif
-     </div>
-   </div>
-   {{ csrf_field() }}
+    <input type="hidden" value="{{ $data['club_code'] }}" name="club_code">
+    <input type="hidden" value="{{ $data['confirmation_status'] }}" name="current_status">
+    <button type="submit" class="btn btn-danger btn-block">ยกเลิกการลงทะเบียน</button>
+    {{ csrf_field() }}
   </fieldset>
 </form>
+@else
+<form class="form-horizontal" method="POST" action="/confirmation.do">
+  <fieldset>
+    <input type="hidden" value="{{ $data['club_code'] }}" name="club_code">
+    <input type="hidden" value="{{ $data['confirmation_status'] }}" name="current_status">
+    <button type="submit" class="btn btn-success btn-block">ยืนยันการลงทะเบียน</button>
+    {{ csrf_field() }}
+  </fieldset>
+</form>
+@endif
 @stop
