ea926ec7f523425b2ea19de54b6f9fe406365c99
theminer3746 <bwsminecraft@hotmail.com>
2016-04-20 11:51:57 +0700

    Add button to move confirmation data to user_year

--- laravel/resources/views/admin/admin.blade.php
+++ laravel/resources/views/admin/admin.blade.php
@@ -2,4 +2,5 @@
 
 @section('content')
 <a class="btn btn-primary btn-lg btn-block" href="/admin/dbmigrate" target="_self">Database migration</a>
+<a class="btn btn-primary btn-lg btn-block" href="/admin/moveconfirmationdata" target="_self">Move Confirmation Data</a>
 @stop
