964469cbab0fcd88639e1b33beea2d8c55d9d49f
theminer3746 <bwsminecraft@hotmail.com>
2016-04-25 23:26:49 +0700

    Add a bunch of check list blade

---
+++ laravel/resources/views/admin/adminCheckList.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.main')
+
+@section('content')
+<a class="btn btn-primary btn-lg btn-block" href="/admin/checklist/beforeconfirmation" target="_self">Before Confirmation</a>
+<a class="btn btn-primary btn-lg btn-block" href="/admin/checklist/afterconfirmation" target="_self">After Confirmation</a>
+<a class="btn btn-primary btn-lg btn-block" href="/admin/checklist/beforeaudition" target="_self">Before Audition</a>
+<a class="btn btn-primary btn-lg btn-block" href="/admin/checklist/afteraudition" target="_self">After Audition</a>
+<a class="btn btn-primary btn-lg btn-block" href="/admin/checklist/beforeregistration" target="_self">Before Registration</a>
+<a class="btn btn-primary btn-lg btn-block" href="/admin/checklist/afterregistration" target="_self">After Registration</a>
+@stop
