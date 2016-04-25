50206658a41eaf434760c29e06f3056a041fbbfb
theminer3746 <bwsminecraft@hotmail.com>
2016-04-25 23:36:41 +0700

    Add general status component

---
+++ laravel/resources/views/admin/layout/component/checkListGeneralStatus.blade.php
@@ -0,0 +1,3 @@
+<span>Release : {{ Config::get('applicationConfig.release') }}</span>
+<span>Mode : {{ Config::get('applicationConfig.mode') }}</span>
+<span>Opeartion Year : {{ Config::get('applicationConfig.operation_year') }}</span>
