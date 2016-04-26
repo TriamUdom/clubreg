5f9ca5a5be8ea8fb14035eb56a63139dde1764a7
theminer3746 <bwsminecraft@hotmail.com>
2016-04-26 08:35:58 +0700

    Add check list to before audition checklist

--- laravel/resources/views/admin/adminBA.blade.php
+++ laravel/resources/views/admin/adminBA.blade.php
@@ -1,5 +1,9 @@
 @extends('layout.main')
 
 @section('content')
-
+<span style="color: #e60722"><b>1. Make sure to move confirmation data to user_year by pressing the "Move Confirmation Data" button on the index</b></span><br>
+<span>2. Make sure that release is set to release</span><br>
+<span>3. Change opeartion mode to audition</span><br>
+<span>4. Make sure that operation_year is set to the correct year</span><br>
+<span>5. Make sure to set administration to false</span>
 @stop
