5610c0d878fe007ed4f6279182c610d555f4540f
theminer3746 <bwsminecraft@hotmail.com>
2016-04-30 01:50:20 +0700

    Fix indentation

--- laravel/resources/views/president/president.blade.php
+++ laravel/resources/views/president/president.blade.php
@@ -3,12 +3,14 @@
 @section('content')
 <a class="btn btn-primary btn-lg btn-block" href="/president/confirmed" target="_self">1. นักเรียนที่ยืนยันชมรมเดิม</a>
 @if($audition)
-<a class="btn btn-primary btn-lg btn-block" href="/president/audition" target="_self">2. นักเรียนที่สมัครออดิชัน</a>
+  <a class="btn btn-primary btn-lg btn-block" href="/president/audition" target="_self">2. นักเรียนที่สมัครออดิชัน</a>
 @else
-<a class="btn btn-primary btn-lg btn-block" href="/president/registration" target="_self">2. นักเรียนที่สมัครชมรม</a>
+  <a class="btn btn-primary btn-lg btn-block" href="/president/registration" target="_self">2. นักเรียนที่สมัครชมรม</a>
 @endif
+
 <a class="btn btn-primary btn-lg btn-block" href="/president/all" target="_self">3. นักเรียนทั้งหมด</a>
 <a class="btn btn-primary btn-lg btn-block" href="/president/setup" target="_self">4. ตั้งค่าชื่อประธานชมรมและครูที่ปรึกษา</a>
+
 @if($canEdit)
   <a class="btn btn-primary btn-lg btn-block" href="/president/fm3301" target="_self">5. Download FM33-01</a>
   <a class="btn btn-primary btn-lg btn-block" href="/president/fm3304" target="_self">6. Download FM33-04</a>
