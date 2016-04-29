8bb28f27230398c12704cfa811a67f9f427d63a2
theminer3746 <bwsminecraft@hotmail.com>
2016-04-30 00:56:03 +0700

    Some button will be disable if president haven't setup the club

--- laravel/resources/views/president/president.blade.php
+++ laravel/resources/views/president/president.blade.php
@@ -2,12 +2,20 @@
 
 @section('content')
 <a class="btn btn-primary btn-lg btn-block" href="/president/confirmed" target="_self">1. นักเรียนที่ยืนยันชมรมเดิม</a>
-@if($data->audition == 1)
+@if($audition)
 <a class="btn btn-primary btn-lg btn-block" href="/president/audition" target="_self">2. นักเรียนที่สมัครออดิชัน</a>
 @else
 <a class="btn btn-primary btn-lg btn-block" href="/president/registration" target="_self">2. นักเรียนที่สมัครชมรม</a>
 @endif
 <a class="btn btn-primary btn-lg btn-block" href="/president/all" target="_self">3. นักเรียนทั้งหมด</a>
-<a class="btn btn-primary btn-lg btn-block" href="/president/fm3301" target="_self">4. Download FM33-01</a>
-<a class="btn btn-primary btn-lg btn-block" href="/president/fm3304" target="_self">5. Download FM33-04</a>
+<a class="btn btn-primary btn-lg btn-block" href="/president/setup" target="_self">4. ตั้งค่าชื่อประธานชมรมและครูที่ปรึกษา</a>
+@if($canEdit)
+  <a class="btn btn-primary btn-lg btn-block" href="/president/fm3301" target="_self">5. Download FM33-01</a>
+  <a class="btn btn-primary btn-lg btn-block" href="/president/fm3304" target="_self">6. Download FM33-04</a>
+  <a class="btn btn-primary btn-lg btn-block" href="/president/fm3305" target="_self">7. แจ้งรายชื่อนักเรียนที่ไม่ผ่านชมรม และ Download FM33-05</a>
+@else
+  <a class="btn btn-primary btn-lg btn-block" href="/president/fm3301" target="_self" disabled>5. Download FM33-01</a>
+  <a class="btn btn-primary btn-lg btn-block" href="/president/fm3304" target="_self" disabled>6. Download FM33-04</a>
+  <a class="btn btn-primary btn-lg btn-block" href="/president/fm3305" target="_self" disabled>7. แจ้งรายชื่อนักเรียนที่ไม่ผ่านชมรม และ Download FM33-05</a>
+@endif
 @stop
