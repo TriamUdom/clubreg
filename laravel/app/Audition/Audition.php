557bd5a88a34d063eec26ebdb7bc8e785ddf9a9e
theminer3746 <bwsminecraft@hotmail.com>
2016-05-01 00:04:58 +0700

    Use double quote instead of quote

--- laravel/app/Audition/Audition.php
+++ laravel/app/Audition/Audition.php
@@ -185,7 +185,7 @@ class Audition{
               ));
 
             if(!is_null(DB::table('user_year')->where('national_id', Session::get('national_id'))->where('year', Config::get('applicationConfig.operation_year'))->pluck('club_code'))){
-              throw new DataException('club_code is not empty cannot proceed');
+              throw new DataException("club_code is not empty cannot proceed");
             }else{
               DB::table('user_year')
                 ->where('national_id', Session::get('national_id'))
