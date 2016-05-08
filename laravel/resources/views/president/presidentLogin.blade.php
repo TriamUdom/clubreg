b24169474164785e09c82816c0924fc3503d7749
theminer3746 <bwsminecraft@hotmail.com>
2016-05-09 01:17:30 +0700

    Add recaptcha to president and admin login

--- laravel/resources/views/president/presidentLogin.blade.php
+++ laravel/resources/views/president/presidentLogin.blade.php
@@ -4,23 +4,27 @@
 <form class="form-horizontal" method="POST" action="/president/login.do">
   <fieldset>
    <legend>เข้าสู่ระบบสำหรับประธานชมรม</legend>
-   @include('layout.component.errornobg')
+   @include('layout.component.errorList')
    <div class="form-group">
      <label for="username" class="col-lg-2 control-label">ชื่อผู้ใช้</label>
      <div class="col-lg-10">
        <input type="text" class="form-control" id="username" name="username">
      </div>
    </div>
-
    <div class="form-group">
      <label for="password" class="col-lg-2 control-label">รหัสผ่าน</label>
      <div class="col-lg-10">
        <input type="password" class="form-control" id="password" name="password">
      </div>
    </div>
+   <div class="form-group">
+     <label class="col-lg-2 control-label"></label>
+     <div class="col-lg-10">
+       {!! Recaptcha::render() !!}
+       <span class="help-block">กดช่องสี่เหลี่ยมให้มีเครื่องหมายถูก</span>
+     </div>
+   </div>
    {{ csrf_field() }}
-   <br />
-
    <div class="form-group">
      <div class="col-lg-10 col-lg-offset-2">
        <button type="submit" class="btn btn-success btn-block">เข้าสู่ระบบ</button>
