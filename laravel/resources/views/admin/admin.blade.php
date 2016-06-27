21c457726e25666e042dff77bbdc159516e559ec
theminer3746 <bwsminecraft@hotmail.com>
2016-06-27 18:08:37 +0700

    Add manual execution button

--- laravel/resources/views/admin/admin.blade.php
+++ laravel/resources/views/admin/admin.blade.php
@@ -4,4 +4,5 @@
 <a class="btn btn-primary btn-lg btn-block" href="/admin/checklist" target="_self">Check List</a>
 <a class="btn btn-primary btn-lg btn-block" href="/admin/dbmigrate" target="_self">Database migration</a>
 <a class="btn btn-primary btn-lg btn-block" href="/admin/moveconfirmationdata" target="_self">Move Confirmation Data</a>
+<a class="btn btn-primary btn-lg btn-block" href="/admin/excmanualregistration" target="_self">Manual registration execution</a>
 @stop
